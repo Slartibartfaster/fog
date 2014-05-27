89ba9fe5eb57c1d49869bc870e192c5e5aa6f218
Slartibartfaster <runslartibartfast@gmail.com>
2014-05-27 11:47:35 -0400

    updated fog tag mocks to include all tagable items

--- lib/fog/aws/requests/compute/create_tags.rb
+++ lib/fog/aws/requests/compute/create_tags.rb
@@ -42,23 +42,39 @@ module Fog
 
           tagged = resources.map do |resource_id|
             type = case resource_id
-            when /^ami\-[a-z0-9]{8}$/i
-              'image'
-            when /^i\-[a-z0-9]{8}$/i
-              'instance'
-            when /^snap\-[a-z0-9]{8}$/i
-              'snapshot'
-            when /^vol\-[a-z0-9]{8}$/i
-              'volume'
-            when /^igw\-[a-z0-9]{8}$/i
-              'internet_gateway'
-            end
-            if type && ((type == 'image' && visible_images[resource_id]) || self.data[:"#{type}s"][resource_id])
+                   when /^ami\-[a-z0-9]{8}$/i
+                     'image'
+                   when /^i\-[a-z0-9]{8}$/i
+                     'instance'
+                   when /^snap\-[a-z0-9]{8}$/i
+                     'snapshot'
+                   when /^vol\-[a-z0-9]{8}$/i
+                     'volume'
+                   when /^igw\-[a-z0-9]{8}$/i
+                     'internet_gateway'
+                   when /^vpc\-[a-z0-9]{8}$/i
+                     'vpc'
+                   when /^rtb\-[a-z0-9]{8}$/i
+                     'route_table'
+                   when /^subnet\-[a-z0-9]{8}$/i
+                     'subnet'
+                   when /^sg\-[a-z0-9]{8}$/i
+                     'security_group'
+                   when /^dopt\-[a-z0-9]{8}$/i
+                     'dhpc_options'
+                   when /^acl\-[a-z0-9]{8}$/i
+                     'network_acls'
+                   when /^eni\-[a-z0-9]{8}$/i
+                     'network_interface'
+                   end
+            if type && self.send("#{type}s".to_sym).send(:get, resource_id)
               { 'resourceId' => resource_id, 'resourceType' => type }
+            elsif type=='security_group' && self.security_groups.all({'group-id' => resource_id}) != []
             else
               raise(Fog::Service::NotFound.new("The #{type} ID '#{resource_id}' does not exist"))
             end
           end
+
 
           tags.each do |key, value|
             self.data[:tags][key] ||= {}
